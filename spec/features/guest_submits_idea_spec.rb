8f11e709f18d4a78590ff4220022cde1b1e480ef
Tommasina <tommasina1@gmail.com>
2016-10-04 16:58:45 -0600

    working on adding list of past ideas in feature test and index.html

--- spec/features/guest_submits_idea_spec.rb
+++ spec/features/guest_submits_idea_spec.rb
@@ -3,6 +3,13 @@ require 'spec_helper'
 describe "Guest submits an idea", :type => :feature, :js => true do
   let(:idea){ Fabricate(:idea) }
   it "creates an idea" do
-    
+    visit root_path
+    fill_in "idea-title", :with => "Help students"
+    fill_in "idea-body", :with => "Within our team"
+    click_link_or_button "create-idea"
+    within('#ideas') do
+      expect(page).to have_content("Help students")
+      expect(page).to have_content("Within our team")
+    end
   end
 end
